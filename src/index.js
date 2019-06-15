38117c627ff1868d5c3e221ad4e5648ee3b3fb9f
Nicolas Pineros <ndpinerosp@unal.edu.co>
2019-06-15 01:43:10 +0000

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+import Color from "./Color";
+import Clock from "./Clock";
+import Request from './Request';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends Component{
+    render(){
+        return(
+            <React.Fragment>
+                <Color/> 
+                <Clock/>
+                <Request/>
+            </React.Fragment>
+        );
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(<App/>,document.getElementById('root'));
